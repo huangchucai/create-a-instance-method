46c0c73bde9644ea2366c81c314a01c140d66892
huangchucai <425205839@qq.com>
2019-06-08 15:29:15 +0800

    test

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -5,7 +5,7 @@ import com.github.hcsp.pet.Cat;
 public class Home {
     public static void main(String[] args) {
         Cat black = new Cat();
-        black.name = "Black";
+        black.name = "ccc";
         black.sayName();
 
         Cat white = new Cat();
